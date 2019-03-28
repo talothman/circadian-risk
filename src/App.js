85286a46da9bacb0664c9d18797efd50be38412f
Talal Alothman <stenfeio@gmail.com>
2019-03-27 23:19:19 -0400

    Add ticker to handle loading names

--- src/App.js
+++ src/App.js
@@ -9,47 +9,88 @@ const Reactangle = () => (
 
 const User = (props) => {
   const name = props.name;
+  const pic = props.pic;
+
   return(
   <div>
+    <img src={pic} />
     {name}
   </div>
   )
 };
 
 const UserList = (props) => {
+  if(!props.users) {
+    return(null)
+  }
+
   const users = props.users;
-  const usersList = users.data.map((user) => {
-    return <User name={user.first_name + ' ' + user.last_name} />    
+  const userList = users.map((user, index) => {
+    return <User key={index} name={user.name.first + ' ' + user.name.last} pic={user.picture.thumbnail} />    
   })
 
   return(
-    usersList
+    userList
   )
 }
 
 class App extends Component {
-  displayUsers=() => {
-    // fetch("https://reqres.in/api/users")
-    // .then((response) => {return response.json()})
-    // .then((data) => {
-
-    // })
-    usersSample.data.map((user) => {
-      
-    })
+  constructor() {
+    super()
+    this.state = {fullUserList: [], renderUserList: [], nameIndex: 1, buttonText: "Load Random User Names!"}
   }
   
+  componentDidMount = () => {
+    fetch('https://randomuser.me/api/?results=100')
+    .then(results => {
+      return results.json();      
+    }).then(data => {
+      this.setState({fullUserList: data.results, renderUserList: null});
+    })
+  }
+
+  displayUsers =() => {
+    if(this.state.nameIndex < this.state.fullUserList.length && !this.timerID) {
+      this.timerID = setInterval(
+        () => this.tick(),
+        1000
+      );
+    } else {
+      this.stopLoadingNames()
+    }
+  }
+  
+  tick = () => {
+    if(this.state.nameIndex < this.state.fullUserList.length) {
+      let nameIndex = this.state.nameIndex;
+      this.setState({
+        renderUserList: this.state.fullUserList.slice(0, nameIndex),
+        nameIndex: ++nameIndex,
+        buttonText: "Stop Loading Random User Names!"
+      })
+    } else {
+      this.stopLoadingNames()
+    }
+  }
+
+  stopLoadingNames = () => {
+    clearInterval(this.timerID);
+    this.timerID = false;
+    this.setState({
+      buttonText: "Load Random User Names!"
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <svg width="800" height="600" id="svg">
-            <Reactangle />
-          </svg>        
-          <button onClick={this.displayUsers}>
-            Click Me!
-          </button>
-          <UserList users={usersSample} />
+        <header className="App-center">
+          <div>
+            <button onClick={this.displayUsers}>
+              {this.state.buttonText}
+            </button>
+          </div>
+          <UserList users={this.state.renderUserList} />
         </header>
       </div>
     );
