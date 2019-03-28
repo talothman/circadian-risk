24bc02ede937b2c423d71ea7eab33a3570dcdb89
Talal Alothman <stenfeio@gmail.com>
2019-03-27 23:18:25 -0400

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const usersSample = {
-  "page": 1,
-  "per_page": 3,
-  "total": 12,
-  "total_pages": 4,
-  "data": [
-      {
-          "id": 1,
-          "first_name": "George",
-          "last_name": "Bluth",
-          "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"
-      },
-      {
-          "id": 2,
-          "first_name": "Janet",
-          "last_name": "Weaver",
-          "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg"
-      },
-      {
-          "id": 3,
-          "first_name": "Emma",
-          "last_name": "Wong",
-          "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg"
-      }
-  ]
-}
-
 const Reactangle = () => (
-  <rect style={{fill: "blue"}} width="100" height="200" x="50" y="20" />
+  <svg width="800" height="600" id="svg">
+    <rect style={{fill: "blue"}} width="100" height="200" x="50" y="20" />  
+  </svg>
 );
 
 const User = (props) => {
